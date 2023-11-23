4d61e0b340393ae63997d1a6ea0be4c38f6e83ce
avinashkrg90 <101476765+avinashkrg90@users.noreply.github.com>
2023-11-23 07:36:38 +0530

    removed the private api key

--- api-data.js
+++ api-data.js
@@ -1,6 +1,3 @@
 let ts = "1681802982683";
 let publicKey = "36885f4feb3f95dd73d6f517d816596b";
 let hashVal = "09f9e26d69cc0068b38a02997cde0e81";
-
-// let publicKey = 36885f4feb3f95dd73d6f517d816596b;
-// let privateKey = b0eafab8b1edd0d8c893daf23c698d1520385905;
